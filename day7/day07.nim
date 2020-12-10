f7c625389787a03ca0c856cf9bc3a0c3f5fe3950
dimspith <51262483+dimspith@users.noreply.github.com>
2020-12-10 02:27:37 +0200

    Solve day 7 part 2

--- day7/day07.nim
+++ day7/day07.nim
@@ -4,9 +4,9 @@ type
   Bag = tuple[amount: int, color: string]
   Bags = seq[Bag]
   BagTable = Table[string, Bags]
-  # Babags = seq[Babag]
+  BagTableTuple = tuple[sol01: BagTable, sol02: BagTable]
 
-proc createBagTable(filename: string): BagTable =
+proc createBagTable(filename: string): BagTableTuple =
   for line in lines filename:
     var
       count: int
@@ -14,10 +14,12 @@ proc createBagTable(filename: string): BagTable =
       bagsStr: string
       bags: Bags
     if scanf(line, "$+ bags contain $+", color1, bagsStr):
+      result[1][color1] = @[]
       for bag in bagsStr.split(", "):
         if scanf(bag, "$i $+ bag", count, color2):
           bags.add((count, color2))
-          result.mgetOrPut(color2, @[]).add (count, color1)
+          result[0].mgetOrPut(color2, @[]).add (count, color1)
+          result[1][color1].add (count, color2)
 
 proc solve01(bagtab: BagTable): int =
   proc checkForBag(bagtab: BagTable, currBag: string, checked: var HashSet[string]) =
@@ -29,7 +31,13 @@ proc solve01(bagtab: BagTable): int =
   var checked: HashSet[string]
   checkForBag(bagtab, "shiny gold", checked)
   result = checked.len
-  echo $checked
+
+proc solve02(bagtab: BagTable, currBag: string): int =
+  if bagtab[currBag].len == 0: return 0
+  for bag in bagtab[currBag]:
+    result += bag[0] * (1 + solve02(bagtab, bag[1]))
+
 
 let bagTable = createBagTable("input07")
-echo solve01(bagTable)
+echo solve01(bagTable[0])
+echo solve02(bagTable[1], "shiny gold")
